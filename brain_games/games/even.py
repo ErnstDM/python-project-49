7197c6d583d6691e23a41290b59d49b760f956a0
ErnstDM <dmitrijernst0@gmail.com>
2024-05-13 16:52:55 +0300

    123

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,12 +4,7 @@ RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_question_and_right_answer():
     question = randint(1, 10)
-
-    if is_even(question):
-        right_answer = 'yes'
-    else:
-        right_answer = 'no'
-    return question, right_answer
+    return question, 'yes' if is_even(question) else 'no'
 
 
 def is_even(question):
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,14 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    if number == 1:
-        return 'no'
-    for i in range(2, int(math.sqrt(number) + 1)):
+    if number < 2:
+        return False
+    for i in range(2, number):
         if number % i == 0:
-            return 'no'
-    return 'yes'
+            return False
+    return True
 
 
 def get_question_and_right_answer():
     question = randint(1, 15)
-    right_answer = is_prime(question)
-    return question, right_answer
+    return question, 'yes' if is_prime(question) else 'no'
